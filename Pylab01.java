ddb692de8172557a90fb819b64b422a2aecbd249
Efelante2124 <rodrihuaranca@hotmail.com>
2023-08-20 15:42:18 -0500

    Update Pylab01.java

--- Pylab01.java
+++ Pylab01.java
@@ -13,7 +13,21 @@ public class Principal {
 		}while(vida < 3 && !fl);
 		if (fl == true)
 			System.out.println("Adivinaste!!!!! ");
+			String gift = Regalo.elegir(vida);
+			System.out.println("Tu regalo es: " + gift);
 		else
 			System.out.println("ups, perdiste... ");
+	}
+}
+
+class Regalo {
+	public static String elegir(int vida) {
+		String gift = "";
+		Switch(vida) {
+		case 1: gift = "Un pasaje al caribe"; break;
+		case 2: gift = "Una visita al museo mas cercano a tu casa"; break;
+		case 3: gift = "Una entrada al cine"; break;
+		}
+		return gift;
 	}
 }
